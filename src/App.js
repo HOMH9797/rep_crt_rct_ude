f8945c7ed67f3ad8b3472c7b13a300cd2b2174bb
Harold <harol.mont97@gmail.com>
2020-03-30 19:51:01 -0500

    este proceso lleva un par ajuste

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import BarraTitulo from './Componentes/BarraTitulo'
+import Highcharts from 'highcharts'
+import moment from 'moment'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+moment.locale('es')
+class App extends Component {
+  state = {
+    registros: [1, 2, 3, 4, 5, 6, 7].map(dia =>
+      [+moment().add(dia, 'days'), Math.random() * 205]
+    )
+  }
+
+  componentDidMount() {
+    this.initGraphic()
+  }
+  initGraphic = () => {
+    Highcharts.chart('grafico', {
+      title: {
+        text: "Mi Registro de peso"
+      },
+      xAxis: {
+        type: "datetime"
+      },
+      series: [{
+        name: "test",
+        data: this.state.registros
+      }]
+    })
+  }
+
+  renderFila = (registro) => {
+    return (
+      <tr key={registro[0]}>
+        <td>{moment(registro[0]).format('LLLL')}</td>
+        <td>{registro[1]}</td>
+      </tr>
+    )
+  }
+  render() {
+    return (
+      <div className="App">
+        <BarraTitulo />
+        <main>
+          <div className="valign-wrapper">
+            <h2>Registro diario</h2>
+          </div>
+          <div className="row">
+
+            <div className="col s6">
+              <div id="grafico" className="z-deph-2 hoverable"></div>
+            </div>
+            <div className="col s6">
+              <table className="z-deph-2 hoverable">
+                <thead>
+                  <tr>
+                    <th>Fecha</th>
+                    <th>Peso (Lbs)</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {
+                    this.state.registros.map(registro => (
+                      this.renderFila(registro)
+                    ))
+                  }
+                </tbody>
+              </table>
+            </div>
+
+          </div>
+        </main>
+        <button className="btn">Click here</button>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Componentes/BarraTitulo.js
@@ -0,0 +1,10 @@
+import React from "react"
+export default () => (
+    <div>
+        <nav>
+            <div className="nav-wrapper">
+                <a href="#" className="brand-logo">Registro Peso</a>
+            </div>
+        </nav>
+    </div>
+)
